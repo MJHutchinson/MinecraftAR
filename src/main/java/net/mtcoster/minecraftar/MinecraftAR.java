aa5e4adc08baf893055b8ac3e872826c57a4b44b
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2015-08-24 11:43:36 +0100

    Still fucking around with annoying shit

--- src/main/java/net/mtcoster/minecraftar/MinecraftAR.java
+++ src/main/java/net/mtcoster/minecraftar/MinecraftAR.java
@@ -1,6 +1,8 @@
 package net.mtcoster.minecraftar;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -11,26 +13,37 @@ import net.mtcoster.minecraftar.proxies.CommonProxy;
  * Created by MTCoster and MJHutchinson. Created August 2015
  */
 
-@Mod(modid = ModInfo.MOD_NAME, version = ModInfo.MOD_VERSION, name = ModInfo.MOD_ID, clientSideOnly = true)
+@Mod(
+    modid = ModInfo.MOD_ID,
+    version = ModInfo.MOD_VERSION,
+    name = ModInfo.MOD_NAME,
+    dependencies = "required-after:Forge@[11.14.1,);"
+)
+
 public class MinecraftAR {
 
-    @Mod.Instance
-    public static MinecraftAR instance = new MinecraftAR();
-
-    @SidedProxy(clientSide = "net.mtcoster.minecraftar.proxies.ClientProxy", serverSide = "net.mtcoster.minecraftar.proxies.ServerProxy")
+    @SidedProxy(
+            clientSide = "net.mtcoster.minecraftar.proxies.ClientProxy",
+            serverSide = "net.mtcoster.minecraftar.proxies.CommonProxy"
+    )
     public static CommonProxy proxy;
 
-    @Mod.EventHandler
+    @Instance(ModInfo.MOD_ID)
+    public static MinecraftAR instance;
+
+    public MinecraftAR(){}
+
+    @EventHandler
     public void preInit(FMLPreInitializationEvent e){
         proxy.preInit(e);
     }
 
-    @Mod.EventHandler
+    @EventHandler
     public void init(FMLInitializationEvent e){
         proxy.init(e);
     }
 
-    @Mod.EventHandler
+    @EventHandler
     public void postInit(FMLPostInitializationEvent e){
         proxy.postInit(e);
     }
--- src/main/java/net/mtcoster/minecraftar/render/items/ItemRenderRegister.java
+++ src/main/java/net/mtcoster/minecraftar/render/items/ItemRenderRegister.java
@@ -1,6 +1,7 @@
 package net.mtcoster.minecraftar.render.items;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemMeshDefinition;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.mtcoster.minecraftar.ModInfo;
@@ -13,14 +14,14 @@ public class ItemRenderRegister {
 
     public static void registerItemRenderer(){
 
-    reg(ModItems.arGogglesItem);
+//    reg(ModItems.arGogglesItem);
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(ModItems.arGogglesItem, 0, new ModelResourceLocation("minecraftar:argoggles_item", "inventory"));
 
     }
 
 
     public static void reg(Item item) {
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
-                .register(item, 0, new ModelResourceLocation(ModInfo.MOD_ID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(ModInfo.MOD_ID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
     }
 
 }
